ac6afa5c662a967155ebc0a3d3b0c89a858caa16
vladstandt <vladstandt@ukr.net>
2026-07-25 19:14:35 +0300

    Create sample_tasks.py

---
+++ data/sample_tasks.py
@@ -0,0 +1,31 @@
+TASKS = [
+
+    {
+        "title": "Finish documentation",
+        "priority": "High",
+        "completed": False,
+        "due": "2026-08-01"
+    },
+
+    {
+        "title": "Buy groceries",
+        "priority": "Low",
+        "completed": True,
+        "due": "2026-07-24"
+    },
+
+    {
+        "title": "Push GitHub updates",
+        "priority": "Medium",
+        "completed": False,
+        "due": "2026-07-29"
+    },
+
+    {
+        "title": "Read Kotlin article",
+        "priority": "Low",
+        "completed": True,
+        "due": "2026-07-20"
+    }
+
+]
