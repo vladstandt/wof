faa226244e480ccc75eeef00fcd52f0430ebeee2
vladstandt <vladstandt@ukr.net>
2026-07-25 19:23:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
-""" Command line task manager """
+from config import EXPORT_FILE
 
-print("Project: todo-cli")
+from repository.task_repository import TaskRepository
+
+from services.task_service import TaskService
+from services.statistics_service import StatisticsService
+from services.priority_service import PriorityService
+
+from renderer.console_renderer import ConsoleRenderer
+from exporters.json_exporter import JsonExporter
+
+tasks = TaskRepository().load()
+
+tasks = TaskService().sort(
+
+    tasks
+
+)
+
+tasks = PriorityService().highest(
+
+    tasks
+
+)
+
+stats = StatisticsService().build(
+
+    tasks
+
+)
+
+ConsoleRenderer().display(
+
+    tasks,
+
+    stats
+
+)
+
+JsonExporter().export(
+
+    tasks,
+
+    EXPORT_FILE
+
+)
